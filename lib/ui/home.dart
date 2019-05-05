5943f058b17a98aa72c464fcaee615789d6fbca3
Gagandeep39 <singh.gagandeep3911@gmail.com>
2019-05-05 10:31:24 +0530

    Initial commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+///Created on Android Studio Canary Version
+///User: Gagandeep
+///Date: 05-05-2019
+///Time: 10:28
+///Project Name: flutter_notes_app
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To-Do App"),
+        backgroundColor: Colors.black38,
+      ),
+    );
+  }
+}
+
